b6c4e9a8031ad784e8e89e17462f391005da9202
Yeferson Mejias <yefer.el.teke@gmail.com>
2023-10-15 18:55:47 -0500

    feat: add icons stack with animations

---
+++ src/modules/portfolio/utils/constants.ts
@@ -0,0 +1,165 @@
+export const BACKENDS_ICONS = [
+  {
+    src: "/portfolio/icons/node.svg",
+    w: "120",
+    h: "46",
+  },
+  {
+    src: "/portfolio/icons/apollo.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/gql.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/msql.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/aws.svg",
+    w: "83",
+    h: "50",
+  },
+  {
+    src: "/portfolio/icons/strapi.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/keystone.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/firebase.svg",
+    w: "48",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/mongo.svg",
+    w: "30",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/redis.svg",
+    w: "68",
+    h: "57",
+  },
+  {
+    src: "/portfolio/icons/sentry.svg",
+    w: "68",
+    h: "60",
+  },
+  {
+    src: "/portfolio/icons/docker.svg",
+    w: "80",
+    h: "57",
+  },
+  {
+    src: "/portfolio/icons/linux.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/vim.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/git.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/turborepo.svg",
+    w: "68",
+    h: "68",
+  },
+];
+
+export const FRONTEND_ICONS = [
+  {
+    src: "/portfolio/icons/ts.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/js.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/html.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/css.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/tailwind.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/mantine.png",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/chakra.png",
+    w: "170",
+    h: "44",
+  },
+  {
+    src: "/portfolio/icons/react.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/rquery.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/next.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/jotai.png",
+    w: "94",
+    h: "32",
+  },
+  {
+    src: "/portfolio/icons/redux.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/pixijs.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/storybook.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/npm.svg",
+    w: "68",
+    h: "68",
+  },
+  {
+    src: "/portfolio/icons/pnpm.svg",
+    w: "68",
+    h: "68",
+  },
+];
